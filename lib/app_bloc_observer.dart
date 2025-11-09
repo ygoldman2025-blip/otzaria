a77af27333ab0598ec422dca2ba80745584a531a
Y-PLONI <7353755@gmail.com>
2025-11-09 22:14:22 +0200

    תיקון עיכוב אינדקס בדיבאגר

--- lib/app_bloc_observer.dart
+++ lib/app_bloc_observer.dart
@@ -1,28 +1,27 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    debugPrint('🔵 ${bloc.runtimeType} Event: $event');
+    print('🔵 ${bloc.runtimeType} Event: $event');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    debugPrint('🟡 ${bloc.runtimeType} Change: $change');
+    print('🟡 ${bloc.runtimeType} Change: $change');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
-    debugPrint('🔴 ${bloc.runtimeType} Error: $error\n$stackTrace');
+    print('🔴 ${bloc.runtimeType} Error: $error\n$stackTrace');
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    debugPrint('🟢 ${bloc.runtimeType} Transition: $transition');
+    print('🟢 ${bloc.runtimeType} Transition: $transition');
   }
 }
