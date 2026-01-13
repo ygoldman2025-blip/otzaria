340661e8ec464429cd0490fd4d13f31a0d423846
Custom Build <custom@build.local>
2026-01-13 18:25:40 +0200

    Remove temporary test files and documentation

--- test_hebrew_url.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001
-echo Testing Hebrew URL...
-echo Running with URL: otzaria://book/ברכות
-"build\windows\x64\runner\Debug\otzaria.exe" "otzaria://book/ברכות"
-pause
--- test_url_direct.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Testing URL scheme directly...
-echo Running: build\windows\x64\runner\Debug\otzaria.exe "otzaria://book/ברכות"
-"build\windows\x64\runner\Debug\otzaria.exe" "otzaria://book/ברכות"
-pause
